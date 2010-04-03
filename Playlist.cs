ec25688436284c5116105c1c8f5e479e871e12c0
Jan Schulte <janschulte90@googlemail.com>
2010-04-03 14:37:05 +0200

    Project and Playlist class created

---
+++ Playlist.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace XML_Playlist_Generator
+{
+    public class Playlist
+    {
+        /// <summary>
+        /// Playlistdokument
+        /// </summary>
+        private XmlDocument doc;
+
+        /// <summary>
+        /// Root-Element
+        /// </summary>
+        private XmlNode root;
+
+        public Playlist(string name)
+        {
+            this.doc = new XmlDocument();
+            //Root Knoten erzeugen
+            this.root = this.doc.CreateElement(name);
+        }
+    }
+}
